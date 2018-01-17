3b5e410f0a77f595b2a136d693419fd48ca6e0c5
rabru <ralf@bruenig.de>
2018-01-17 18:42:47 +0100

    Split ha deployment
    Basic deployment per Device
    HA setup on one Device
    No key exchange for ssh needed

---
+++ scripts/onboarding_udf_dmzlb_ha-setup.sh
@@ -0,0 +1,87 @@
+######## Manual Tasks on the host ######
+# Please execute the following two lines on the host BIG-IP,
+# on which you plan to start this script from the BIG-IQ
+#
+# Enable bash script for admin on both bigips, additional change password of admin for UDF:
+# (tmos) # modify auth user admin password admin shell bash
+#
+# UDF specific: Disabeling hostname update over UDF
+# (tmos) # modify sys management-dhcp sys-mgmt-dhcp-config request-options delete { domain-name domain-name-servers ntp-servers host-name }
+# (tmos) # modify sys global-settings hostname bigip1
+# (tmos) # save sys config partitions all
+# > full_box_reboot
+
+######## VARS ##########################
+PEER_MGMT_IP="10.1.1.15"
+
+ADMIN_PASSWD="admin"
+
+# System
+HOSTNAME="dmzlb1.f5demo.com"
+HOSTNAME_PEER="dmzlb2.f5demo.com"
+DEVICE_GROUP_NAME="dmzlb_cluster"
+# DNS
+
+
+# Network
+EXT_SELF_IP_MASK="24"
+EXT_SELF_IP="10.1.10.20"
+
+#######################################
+# Network
+tmsh create /net self ext_floating_self address $EXT_SELF_IP/$EXT_SELF_IP_MASK vlan external traffic-group traffic-group-1
+
+######################
+# HA Setup
+
+# setup trust
+tmsh modify cm trust-domain Root ca-devices add { $PEER_MGMT_IP } name $HOSTNAME_PEER username admin password $ADMIN_PASSWD
+# Create Device Group
+tmsh create cm device-group $DEVICE_GROUP_NAME  devices add { $HOSTNAME $HOSTNAME_PEER } type sync-failover network-failover enabled
+# Wait until ready to sync
+echo "Wait until ready to sync:"
+for i in {0..60..2}
+do
+  value=$( tmsh show cm sync-status | grep Color | grep -ic blue )
+  echo "Wait $i seconds"
+  if [ $value -eq 1 ]
+  then
+    echo " - Ready for Sync - "
+    break
+  else
+    if [ $i -eq 60 ]
+    then
+      echo " - Not ready to sync - Exit - "
+      exit
+    fi
+    sleep 2
+  fi
+done
+# Initial Sync
+tmsh run cm config-sync force-full-load-push to-group $DEVICE_GROUP_NAME
+# Wait until sync is finished
+echo "Wait until sync is finished:"
+for i in {0..60..2}
+do
+  value=$( tmsh show cm sync-status | grep Color | grep -ic green )
+  echo "Wait $i seconds"
+  if [ $value -eq 1 ]
+  then
+    echo " - Sync is done - "
+    break
+  else
+    if [ $i -eq 60 ]
+    then
+      echo " - Sync Failed - "
+    fi
+    sleep 2
+  fi
+done
+
+# Show Sync status
+tmsh show cm sync-status
+ssh admin@$PEER_MGMT_IP tmsh show cm sync-status
+
+# save changes
+tmsh save /sys config
+ssh admin@$PEER_MGMT_IP tmsh save /sys config
